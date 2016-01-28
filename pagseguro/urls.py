393ae323e7b1ec66de9c1f133ff55865f5534051
Luiz Henrique Freitas <Luiz Henrique Freitas>
2016-01-28 16:17:22 -0200

    Fix URLS Messages for Django 1.10

--- pagseguro/urls.py
+++ pagseguro/urls.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url
+from pagseguro import views
 
-urlpatterns = patterns(
-    'pagseguro.views',
-
-    url(
-        r'^$', 'receive_notification', name='pagseguro_receive_notification'
-    ),
-
-)
+urlpatterns = [
+     url(r'^$', views.receive_notification, name='pagseguro_receive_notification'),
+]
